0dfb9d03d7389a82b293c19e267fd6b6de17eb65
xtra1n <aleontyev96@gmail.com>
2024-09-29 17:18:42 +0300

    lab4

---
+++ 1.py
@@ -0,0 +1,13 @@
+# # x=int(input())
+# # min=-1e8
+# # max=-1e8
+# # while x != 0:
+# #     if max < x:
+# #         max=x
+# #     if x > min and x < max:
+# #         min = x
+# #     x=int(input())
+# # print(min)
+
+
+print(f'{"dfd":^{16}}')
